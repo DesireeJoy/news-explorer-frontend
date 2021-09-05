de5836df2f576ab0b5b5c0985f5e684dfed80073
DesireeJoy <desireebradish@gmail.com>
2021-09-04 21:37:41 -0400

    https

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -117,7 +117,7 @@ class MainApi {
 
 export const mainApi = new MainApi({
     // baseUrl: 'https://api.desinews.students.nomoreparties.site/',
-    baseUrl: 'http://34.125.192.74:3000/',
+    baseUrl: 'https://34.125.192.74:3000/',
     headers: {
       'Accept': "application/json",
     'Content-Type': 'application/json',
